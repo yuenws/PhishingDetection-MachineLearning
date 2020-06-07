382f35b079a55a03a0c61002c47ecff7c03b3779
yuenws <36186777+yuenws@users.noreply.github.com>
2020-06-08 04:55:22 +0800

    Update clientServer.php

--- Code/clientServer.php
+++ Code/clientServer.php
@@ -6,6 +6,6 @@ $html = file_get_contents($site);
 
 $bytes=file_put_contents('markup.txt', $html);
 
-$decision=exec("C:/Users/Ruby/AppData/Local/Programs/Python/Python37/python result4chrome.py $site 2>&1 ");
+$decision=exec(" /*update your own python path*/ $site 2>&1 ");
 echo $decision;
 ?>
